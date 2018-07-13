e771add143932530702d6db624f0875884fd4850
makusu <vloxomire@gmail.com>
2018-07-12 21:59:29 -0300

    vinculacion botones
    Se vincular y sobrescribieron los botones del primer linear layout

--- app/src/main/java/com/example/itmaster/ejercicio_layout_completo/MainActivity.java
+++ app/src/main/java/com/example/itmaster/ejercicio_layout_completo/MainActivity.java
@@ -2,12 +2,25 @@ package com.example.itmaster.ejercicio_layout_completo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button boton1, boton2, boton3, boton4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        boton1 = findViewById(R.id.boton1);
+        boton2 = findViewById(R.id.boton2);
+        boton3 = findViewById(R.id.boton3);
+        boton4 = findViewById(R.id.boton4);
+
+        boton1.setText("Rambo2");
+        boton2.setText("Rocky2");
+        boton3.setText("Ghost2");
+        boton4.setText("E.T.2");
     }
 }
